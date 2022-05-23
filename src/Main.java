85969f03cc023d674d3c6e4f313a0822ee431066
Olga <olga.diaz-gutierrez@vontobel.com>
2022-05-23 18:45:01 +0200

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
 public class Main {
   public static void main(String[] args) {
-    final var input = "0123456789";
-
-    StringBuilder output = new StringBuilder();
-    for (int i = input.toCharArray().length - 1; i >= 0; i--) {
-      output.append(input.charAt(i));
-    }
-
-    System.out.println(output);
   }
 }
